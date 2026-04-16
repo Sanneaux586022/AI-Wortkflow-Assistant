e1de337f6a9903a72015d3b26f5dfbd3993e3814
Pascal Sanama <sanneauxhf@gmail.com>
2026-04-17 00:16:25 +0200

    Corregge docker_entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 flask db upgrade 
 
-exec gunicorn --bind 0.0.0.0:${PORT:-5000} --workers 4 "main:create_app()"
+exec gunicorn --bind 0.0.0.0:${PORT:-5000} --workers 4 "main:app"
