9f8869a5784676bccabe6208c42b2df742a20dd8
Pascal Sanama <sanneauxhf@gmail.com>
2026-04-20 23:58:57 +0200

    fix: la porta esposta torna la 5000

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,8 @@ set -e
 
 echo "PORT value: ${PORT}"
 echo "Starting flask db upgrade..."
+# export FLASK_APP=main.py
 flask db upgrade
 
-echo "Starting gunicorn on port ${PORT:-10000}..."
-exec gunicorn --bind 0.0.0.0:${PORT:-10000} --workers 4 --log-level debug "main:app"
+echo "Starting gunicorn on port ${PORT:-5000}..."
+exec gunicorn --bind 0.0.0.0:${PORT:-5000} --workers 4 --log-level debug "main:app"
